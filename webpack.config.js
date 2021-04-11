1644f17c9cff49c90b1a74fd2edfb2049af9e8d5
Dailon Miguel Reichert <dailonreichert@gmail.com>
2021-04-11 19:29:30 -0300

    end of configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+    mode   : isDevelopment ? 'development' : 'production',
+
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+
     entry  : path.resolve(__dirname, 'src', 'index.jsx'),
     
     output : {
@@ -12,12 +19,27 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
 
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+
     module : {
         rules: [
             {
                 test   : /\.jsx$/,
                 exclude: /node_modules/,
                 use    : 'babel-loader'
+            },
+            {
+                test   : /\.scss$/,
+                exclude: /node_modules/,
+                use    : ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
